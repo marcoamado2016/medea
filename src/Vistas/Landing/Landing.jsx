dec6e8e4f1b613ff440e39209d219e0259aab435
Nicolas <urcocarp1986.nr@gmail.com>
2024-11-04 00:23:08 -0300

    landing

--- src/Vistas/Landing/Landing.jsx
+++ src/Vistas/Landing/Landing.jsx
@@ -1,29 +1,29 @@
 import './Landing.css';
 import radio from '../../assets/index_14.jpg';
 import tv from '../../assets/index_16.jpg';
-import banner from '../../assets/banner-01.jpg';
+import fondo from '../../assets/home_02.jpg';
 
 const Landing = () => {
   return (
-    <div>
-      <div className='nube-principal'>
-        <img src={banner} alt="Banner principal" />
-      </div>
-      <div className='medios'>
-        <div className='medios-tittle'>
-          <h1>Aqui estamos</h1>
-        </div>
-        <div className='medios-imagenes'>
-          <img src={radio} alt="Imagen de radio" />
-          <img src={tv} alt="Imagen de televisión" />
-        </div>
-        <div>
-          <a>
+    <div className='medios'>
 
-          </a>
-        </div>
-      </div>
+    <div className='iconos'>
+    <div className='titulo'>
+      <h1>AQUÍ ESTAMOS</h1>
     </div>
+    <div>
+      <img src={tv}></img>
+      <img src={radio}></img>
+    </div>
+    <div className='texto-Landing'>
+        <a>El Ministerio M.E.D.E.A , con 41 años de misión, difunde el evangelio globalmente mediante tecnología y medios, alcanzando a quienes no puedan asistir en persona. Brinda apoyo espiritual a personas privadas de libertad y mantiene una presencia continua a través de "Libertad en la red" y "Un mensaje al Corazon". Su labor busca transformar vidas con mensaje de esperanza y amor a Dios</a>
+
+    </div>
+
+    </div>
+     
+      
+     </div>
   );
 };
 
