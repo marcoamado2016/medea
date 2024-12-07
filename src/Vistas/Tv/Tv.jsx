fa088dec9b9354ab579b35af5479dda2e111c4d8
Nicolas <urcocarp1986.nr@gmail.com>
2024-12-07 15:01:54 -0300

    tv

--- src/Vistas/Tv/Tv.jsx
+++ src/Vistas/Tv/Tv.jsx
@@ -4,22 +4,28 @@ import portada from '../../assets/portada.png'
 
 const Tv =()=>{
     return(
-        <div className={style.tvContainer}>
-           <img src={portada} className={style.imgPortada}></img>
+    <div className={style.tvContainer}>
+        <section className={style.firstSection}>
+         <img src={portada} className={style.imgPortada}></img>
            <div className={style.Links}>
-           <ul>
-              <li>
-                  <a>MENSAJE</a>
-              </li>
-              <li>
-                  <a>EVENTO</a>
-              </li>
-              <li>
-                  <a>ARTICULOS</a>
-              </li>
-           </ul>
+            
+                    <a>MENSAJE</a>
+                
+                    <a>EVENTO</a>
+            
+                    <a>ARTICULOS</a>
+            
+        
 
            </div>
+        </section>
+          <section className={style.word}>
+           <h1>El poder de la palabra</h1>
+           <div className={style.words}>
+            <h2>Raul Villareal</h2>
+            <h2> 24.11.2024</h2>
+           </div>
+          </section>
 
         </div>
 
