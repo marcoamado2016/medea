6df842e643fd54b9e59284dcb72c551dabd2d2f4
Nicolas <urcocarp1986.nr@gmail.com>
2024-12-11 21:08:06 -0300

    listo mensaje

--- src/Vistas/Mensaje/Mensaje.jsx
+++ src/Vistas/Mensaje/Mensaje.jsx
@@ -5,12 +5,20 @@ import style from  './Mensaje.module.css'
 const Mensaje =()=>{
     return(
         <div className={style.ContainerForm}>
-         <section className={style.Section}>
-              <h1>no estas solo</h1>
-            <div>
-              <imput></imput>
-              <input></input>
-
+         <section className={style.SectionForm}>
+         <div className={style.tituloForm}>
+              <h1>No estas solo</h1>
+         </div>
+            <div className={style.InputForm}>
+              <input type="text" placeholder="Nombre" ></input>
+              <input type="text" placeholder="Telefono"></input>
+              <textarea
+            placeholder="Petición"
+            className={`${style.InputField} ${style.TextArea}`}
+          ></textarea>
+            </div>
+            <div className={style.ButtonContainer}>
+            <button className={style.SubmitButton}>Enviar</button>
             </div>
          </section>
         </div>
