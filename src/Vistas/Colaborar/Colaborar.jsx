86c7253f8c32f048a0bedf76816a8eb628732205
Nicolas <urcocarp1986.nr@gmail.com>
2024-12-26 22:43:19 -0300

    estosomos

---
+++ src/Vistas/Colaborar/Colaborar.jsx
@@ -0,0 +1,12 @@
+import style from "./Colaborar.module.css"
+
+
+const Colaborar = () => {
+    return(
+        <div className={style.colaborar}>
+
+
+        </div>
+    )
+}
+export default Colaborar;
