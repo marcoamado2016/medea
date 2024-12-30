1b3a189162ef2ba22b3d0e3bcd9ea7561ebc7281
Nicolas <urcocarp1986.nr@gmail.com>
2024-12-30 09:07:41 -0300

    mensajes

--- src/Vistas/Mensaje/Mensaje.jsx
+++ src/Vistas/Mensaje/Mensaje.jsx
@@ -1,27 +1,70 @@
-import style from  './Mensaje.module.css'
+import React, { useState } from 'react';
+import style from './Mensaje.module.css';
 
+const Mensaje = () => {
+  const [formData, setFormData] = useState({
+    nombre: '',
+    telefono: '',
+    peticion: '',
+  });
 
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+  };
 
-const Mensaje =()=>{
-    return(
-        <div className={style.ContainerForm}>
-         <section className={style.SectionForm}>
-         <div className={style.tituloForm}>
-              <h1>No estas solo</h1>
-         </div>
-            <div className={style.InputForm}>
-              <input type="text" placeholder="Nombre" ></input>
-              <input type="text" placeholder="Telefono"></input>
-              <textarea
+  const handleSubmit = () => {
+    
+    console.log('Formulario enviado:', formData);
+
+    
+    setFormData({
+      nombre: '',
+      telefono: '',
+      peticion: '',
+    });
+  };
+
+  return (
+    <div className={style.ContainerForm}>
+      <section className={style.SectionForm}>
+        <div className={style.tituloForm}>
+          <h1>No estás solo</h1>
+        </div>
+        <div className={style.InputForm}>
+          <input
+            type="text"
+            name="nombre"
+            placeholder="Nombre"
+            value={formData.nombre}
+            onChange={handleInputChange}
+          />
+          <input
+            type="text"
+            name="telefono"
+            placeholder="Teléfono"
+            value={formData.telefono}
+            onChange={handleInputChange}
+          />
+          <textarea
+            name="peticion"
             placeholder="Petición"
             className={`${style.InputField} ${style.TextArea}`}
+            value={formData.peticion}
+            onChange={handleInputChange}
           ></textarea>
-            </div>
-            <div className={style.ButtonContainer}>
-            <button className={style.SubmitButton}>Enviar</button>
-            </div>
-         </section>
         </div>
-    )
-}
+        <div className={style.ButtonContainer}>
+          <button className={style.SubmitButton} onClick={handleSubmit}>
+            Enviar
+          </button>
+        </div>
+      </section>
+    </div>
+  );
+};
+
 export default Mensaje;
