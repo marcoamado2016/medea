7832ede4b32f792590bf49821d0f0f751474ca2f
Nicolas <urcocarp1986.nr@gmail.com>
2024-12-30 11:59:39 -0300

    pdf

--- src/Vistas/Tv/Tv.jsx
+++ src/Vistas/Tv/Tv.jsx
@@ -1,36 +1,29 @@
-import style from './Tv.module.css'
-import portada from '../../assets/portada.png'
+import { Link } from 'react-router-dom';
+import style from './Tv.module.css';
+import portada from '../../assets/portada.png';
 
-
-const Tv =()=>{
-    return(
-    <div className={style.tvContainer}>
-        <section className={style.firstSection}>
-        <a href="https://www.youtube.com/@MEDEATVHD" target="_blank" rel="noopener noreferrer">
-          <img src={portada} className={style.imgPortada} ></img>
-        </a>
-           <div className={style.Links}>
-            
-                    <a>MENSAJE</a>
-                
-                    <a>EVENTO</a>
-            
-                    <a>ARTÍCULOS</a>
-            
-        
-
-           </div>
-        </section>
-          <section className={style.word}>
-           <h1>El poder de la palabra</h1>
-           <div className={style.words}>
-            <h2>Raul Villareal</h2>
-            <h2> 24.11.2024</h2>
-           </div>
-          </section>
-
+const Tv = () => {
+    return (
+        <div className={style.tvContainer}>
+            <section className={style.firstSection}>
+                <a href="https://www.youtube.com/@MEDEATVHD" target="_blank" rel="noopener noreferrer">
+                    <img src={portada} className={style.imgPortada} alt="Portada" />
+                </a>
+                <div className={style.Links}>
+                    <Link to="/mensaje">MENSAJE</Link>
+                    <a href="/eventos">EVENTO</a>
+                    <a href="/dist/pdf/Diosguarda.pdf" download>ARTÍCULOS</a>
+                </div>
+            </section>
+            <section className={style.word}>
+                <h1>El poder de la palabra</h1>
+                <div className={style.words}>
+                    <h2>Raul Villareal</h2>
+                    <h2>24.11.2024</h2>
+                </div>
+            </section>
         </div>
+    );
+};
 
-    )
-}
-export default Tv
+export default Tv;
