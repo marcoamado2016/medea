dd72ba80359b987fabf76b0f8c2bb20fa58deee8
Marco Amado <marco.amado@adviters.com>
2025-12-06 02:47:49 -0300

    r

--- src/Componentes/Concejeria/Concejeria.jsx
+++ src/Componentes/Concejeria/Concejeria.jsx
@@ -3,34 +3,42 @@ import { useNavigate } from 'react-router'
 
 
 
-const Concejeria=()=>{
-    const navigate= useNavigate();
-    const handleNavigate =()=>{
+const Concejeria = () => {
+    const navigate = useNavigate();
+    const handleNavigate = () => {
         navigate('/mensaje')
     }
-    return(
-        <div className={style.concejeria}>
-            <div className={style.texto}>
-                <h1>No estás solo</h1>
-                <p>
-                    Orar por los demas es un acto profundo de
-                    amor por el prójimo, nos permite desconectarnos
-                    de las distracciones diarias para pensar en el
-                    bienestar de otros. No son solo palabras, se 
-                    trata de desear lo mejor desde el corazon, for-
-                    taleciendo asi los lazos humanos. Para muchos,
-                    la oración es un recordatorio de que no estamos
-                    solos y nos conecta Dios, con los demás y con
-                    nosotros mismos. Es un gesto simple pero 
-                    poderosoque transforma tanto a quienes oran 
-                    como aquellos por quienes se ora
-                </p>
-               <button  onClick={handleNavigate} className={style.boton}>LO NECESITO</button>
+    return (
+        <>
+            <div className={style.concejeria}>
+                <div className={style.texto}>
+                    <h1>No estás solo</h1>
+                    <p>
+                        Orar por los demas es un acto profundo de
+                        amor por el prójimo, nos permite desconectarnos
+                        de las distracciones diarias para pensar en el
+                        bienestar de otros. No son solo palabras, se
+                        trata de desear lo mejor desde el corazon, for-
+                        taleciendo asi los lazos humanos. Para muchos,
+                        la oración es un recordatorio de que no estamos
+                        solos y nos conecta Dios, con los demás y con
+                        nosotros mismos. Es un gesto simple pero
+                        poderosoque transforma tanto a quienes oran
+                        como aquellos por quienes se ora
+                    </p>
+                    <button onClick={handleNavigate} className={style.boton}>LO NECESITO</button>
+                </div>
+                <button className={style.botonFlotante}
+                    onClick={() => window.open('https://wa.link/s6hyvz', '_blank')}
+                >oramos por <b>vos!</b></button>
             </div>
-          <button className={style.botonFlotante}
-         onClick={() => window.open('https://wa.link/s6hyvz', '_blank')}
-          >oramos por <b>vos!</b></button>
-        </div>
+            <br />
+            <br />
+            <br />
+             <br />
+        </>
+
+
     )
 
 }
