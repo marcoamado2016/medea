280514c8c662e94fa78564f2c3d84805fcd48bcb
marco.amado <marcos242016@gmail.com>
2026-08-08 01:02:19 -0300

    07/08/2026

--- src/Vistas/Colaborar/Colaborar.jsx
+++ src/Vistas/Colaborar/Colaborar.jsx
@@ -1,8 +1,11 @@
 import style from "./Colaborar.module.css";
 import texto from "../../assets/TEXTOColaborar.png";
 import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 
 const Colaborar = () => {
+  const navigate = useNavigate();
+
   useEffect(() => {
     window.scrollTo(0, 0);
   }, []);
@@ -11,15 +14,20 @@ const Colaborar = () => {
     window.scrollTo({ top: 0, behavior: "smooth" });
   };
 
+  const goBack = () => {
+    navigate(-1);
+  };
+
   return (
     <div className={style.colaborar}>
+      <button type="button" className={style.backButton} onClick={goBack}>
+        ← Volver
+      </button>
       <div className={style.texto} onClick={upScroll}>
-        <img
-          src={texto}
-          style={{ width: "400px", height: "450px", marginTop: "100px",marginLeft:"-400px" }}
-        ></img>
+        <img src={texto} alt="Colaborar" className={style.imgColaborar} />
       </div>
     </div>
   );
 };
 export default Colaborar;
+
